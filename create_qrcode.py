d80bed5c8f5f2e40867bbae25ee6c012428de3fd
Masamitsu MURASE <masamitsu.murase@gmail.com>
2020-05-31 01:07:19 +0900

    Prepare for initial release.

--- create_qrcode.py
+++ create_qrcode.py
@@ -18,17 +18,51 @@ def create_url(base_url, param):
     return base_url + "#" + encode_param(param)
 
 
-base_url = "http://localhost:8000/src/qr_index.html"
+base_url = "https://masamitsu-murase.github.io/seats_ar/src/qr_index.html"
 param = {
     "users": [
-        ("村瀬", (3, 5)),
-        ("昌満", (-13, 0.3)),
+        ("Murase", (0, 0)),
+        ("Masamitsu", (2, 2)),
     ],
 
     "markers": [
+        (-1, 1),
+        (-3, -2),
         (5, 5),
         (-3, -2),
+        (5, 5),
+        (-3, -2),
+        (5, 5),
+        (-3, -2),
+        (5, 5),
+        (-1, -1),
+        (1, 1),
+        (1, -1),
+        (5, 5),
+        (-3, -2),
+        (5, 5),
+        (-3, -2),
+        (5, 5),
+        (-3, -2),
+        (5, 5),
+        (-3, -2),
+        (5, 5),
+        (-3, -2),
+        (5, 5),
+        (-3, -2),
+        (5, 5),
+        (0, 0),
     ]
 }
 
-print(create_url(base_url, param))
+url = create_url(base_url, param)
+print(url)
+qr = qrcode.QRCode(
+    error_correction=qrcode.constants.ERROR_CORRECT_M,
+    box_size=2,
+    border=8
+)
+qr.add_data(url)
+qr.make()
+img = qr.make_image()
+img.save('hoge.png')
