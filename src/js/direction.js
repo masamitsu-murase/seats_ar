e809d31b01f40d589ca9ca9fb8387408fa081c55
Masamitsu MURASE <masamitsu.murase@gmail.com>
2020-06-29 01:15:57 +0900

    Implement distance.

--- src/js/direction.js
+++ src/js/direction.js
@@ -27,26 +27,54 @@
         return data;
     };
 
+    const createElement = function (name, attrs) {
+        const elem = document.createElement(name);
+        if (attrs) {
+            for (const [key, value] of Object.entries(attrs)) {
+                elem.setAttribute(key, value);
+            }
+        }
+        return elem;
+    };
+
     var constructMarkers = function (directions) {
         const angle_offset = -90;  // arrow indicates y-axis direction.
+        const distance_unit = 0.5; // 0.5m
         var root = $("#root");
         var camera = $("#camera");
         directions.forEach((direction, index) => {
             var rad = Math.atan2(direction[1], direction[0]);
             var angle = rad * 180 / Math.PI + angle_offset;
 
-            var marker = document.createElement("a-marker");
-            marker.setAttribute("type", "barcode");
-            marker.setAttribute("value", `${index + 1}`);
+            var marker = createElement("a-marker", {
+                "type": "barcode",
+                "value": `${index + 1}`
+            });
 
-            var entity = document.createElement("a-entity");
-            entity.setAttribute("position", "0 0 0");
-            entity.setAttribute("scale", "1 1 1");
-            entity.setAttribute("rotation", `0 ${angle} 0`);
-            entity.setAttribute("gltf-model", "#arrow");
+            var entity = createElement("a-entity", {
+                "position": "0 0 0",
+                "scale": "1 1 1",
+                "rotation": `0 ${angle} 0`,
+                "gltf-model": "#arrow"
+            });
+
+            const distance = Math.sqrt(direction[0] ** 2 + direction[1] ** 2) * distance_unit;
+            const rounded_distance = Math.round(distance * 10);
+            const distance_str = `${Math.floor(rounded_distance / 10)}.${rounded_distance % 10}`;
+            const distance_rad = -(rad - Math.PI);
+            const position = `${Math.cos(distance_rad)} 0.1 ${Math.sin(distance_rad)}`;
+            var text = createElement("a-text", {
+                "value": `${distance_str}m`,
+                "align": "center",
+                "scale": "2 2 2",
+                "rotation": `-70 ${angle} 0`,
+                "position": position,
+                "color": "#FF8888"
+            });
 
             root.insertBefore(marker, camera);
             marker.appendChild(entity);
+            marker.appendChild(text);
         });
     };
 
